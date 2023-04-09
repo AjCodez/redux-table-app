b0375f85c680dedc91d930448e0e3bc15aeb69a9
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2023-04-10 01:33:18 +0530

    saga

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,12 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
+import createSagaMiddleware from "redux-saga";
 
+const sagaMiddleware = createSagaMiddleware();
 
 const reducer = combineReducers({
     user: userSlice,
 });
 const store = configureStore({
     reducer,
+
 });
 
 export default store;
